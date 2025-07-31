9a4901603dce9e30c265f44d39402a26f04c13eb
Christopher Moran <chris.moran@emergent.com.au>
2025-07-31 15:34:06 +1000

    update terraform virgin

--- terraform/imagebuilder/Main.tf
+++ terraform/imagebuilder/Main.tf
@@ -2,12 +2,12 @@ terraform {
     required_providers {
       aws = {
         source = "hashicorp/aws"
-        version = "~> 5.0"
+        version = "~> 6.0"
       }
     }
     backend "s3" {
       bucket = "cfm-tf-state"
-      key = "hilife-imagebuilder/state"
+      key = "openvms-imagebuilder/state"
       region = "ap-southeast-2"
     }
 }
